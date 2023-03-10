7cfbbf28970ebe3a0eaf2bdc00338ed3bf7d4809
princesszion <maikemvictorine@gmail.com>
2023-03-11 01:52:33 +0200

    new files

--- app.py
+++ app.py
@@ -1,34 +1,95 @@
-import numpy as np
+# import numpy as np
+# import pandas as pd
+# from flask import Flask, request, render_template
+# from sklearn import preprocessing
+# import pickle
+
+# app = Flask(__name__)
+# model = pickle.load(open('model.pkl', 'rb'))
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     feature_list = request.form.to_dict()
+#     feature_list = list(feature_list.values())
+#     feature_list = list(map(int, feature_list))
+#     final_features = np.array(feature_list).reshape(1, 22)
+
+#     prediction = model.predict(final_features)
+#     output = int(prediction[0])
+#     # if output == 1:
+#     #     text = ">50K"
+#     # else:
+#     #     text = "<=50K"
+
+#     return render_template('index.html', prediction_text='Player Rating is {}'.format(output))
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+import streamlit as st 
 import pandas as pd
-from flask import Flask, request, render_template
-from sklearn import preprocessing
-import pickle
+import numpy as np
+from prediction import predict
 
-app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+st.title('Player Ratings')
+st.markdown('Enter proposed values to predict player rating')
 
+st.header('Player Features')
+col1, col2 = st.columns(2)
+with col1:
+    movement_reactions = st.slider('movement_reactions', 10, 50, 100)
+    mentality_composure = st.slider('mentality_composure', 10, 50, 100)
+    passing   = st.slider('passing', 10, 50, 100)
+    potential = st.slider('potential', 10, 50, 100)
+    dribbling = st.slider( 'dribbling',10, 50, 100)
+    wage_eur = st.slider('wage_eur', 10, 50, 100)
+    power_shot_power = st.slider( 'power_shot_power',10, 50, 100)
+    value_eur = st.slider('value_eur',10, 50, 100)
+    release_clause_eur = st.slider('release_clause_eur', 10, 50, 100)
+    attacking_short_passing = st.slider('attacking_short_passing', 10, 50, 100)
+    skill_long_passing = st.slider('skill_long_passing', 10, 50, 100)
+with col2:
+    physic = st.slider('physic', 10, 50, 100)
+    ls = st.slider('ls', 10, 50, 100)
+    international_reputation = st.slider('international_reputation', 10, 50, 100)
+    mentality_aggression = st.slider('mentality_aggression', 10, 50, 100)
+    #dribbling = st.slider( 'dribbling',10, 50, 100)
+    attacking_crossing = st.slider('attacking_crossing', 10, 50, 100)
+    skill_ball_control = st.slider( 'skill_ball_control',10, 50, 100)
+    age = st.slider('age',10, 50, 100)
+    shooting = st.slider('shooting ', 10, 50, 100)
+    skill_curve = st.slider('skill_curve', 10, 50, 100)
+    power_long_shots = st.slider('power_long_shots', 10, 50, 100)
+    
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    feature_list = request.form.to_dict()
-    feature_list = list(feature_list.values())
-    feature_list = list(map(int, feature_list))
-    final_features = np.array(feature_list).reshape(1, 12)
-
-    prediction = model.predict(final_features)
-    output = int(prediction[0])
-    if output == 1:
-        text = ">50K"
-    else:
-        text = "<=50K"
-
-    return render_template('index.html', prediction_text='Employee Income is {}'.format(text))
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    if st.button("Predict Player"):
+        result = predict(np.array([['movement_reactions',
+ 'mentality_composure',
+ 'passing',
+ 'potential',
+ 'dribbling',
+ 'wage_eur',
+ 'power_shot_power',
+ 'value_eur',
+ 'release_clause_eur',
+ 'mentality_vision',
+ 'attacking_short_passing',
+ 'skill_long_passing',
+ 'physic',
+ 'ls',
+ 'international_reputation',
+ 'skill_ball_control',
+ 'age',
+ 'shooting',
+ 'skill_curve',
+ 'power_long_shots',
+ 'attacking_crossing',
+ 'mentality_aggression']]))
+        
+        st.text(result[0])
